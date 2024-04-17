e65fd2ae58a5493a5a3a9c80c9c5aae3a29ddf3e
Function <42390852+algofunction@users.noreply.github.com>
2024-04-17 23:57:24 +0200

    Create ft_ultimate_ft.c

---
+++ Funda/D01/C01/ft_ultimate_ft.c
@@ -0,0 +1,16 @@
+/*Instructions
+
+Ecrire une fonction qui prend un pointeur sur pointeur sur pointeur sur poin-
+teur sur pointeur sur pointeur sur pointeur sur pointeur sur pointeur sur int en
+paramètre et donne à l’int la valeur de 42.
+*/
+
+
+
+// Solution
+#include <unistd.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
