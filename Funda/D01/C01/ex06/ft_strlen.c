62e54321bd3980c6acd6ba1ab826e951a89f871e
Function <42390852+algofunction@users.noreply.github.com>
2024-04-27 23:34:20 +0200

    Update ft_strlen.c

--- Funda/D01/C01/ex06/ft_strlen.c
+++ Funda/D01/C01/ex06/ft_strlen.c
@@ -2,5 +2,18 @@
 
 Écrire une fonction qui compte le nombre de caractères dans 
 une chaîne de caractères et qui retourne le nombre trouvé.
-
 */
+
+#include <unistd.h>
+
+int		ft_strlen(char *str)
+{
+	int counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
+}
