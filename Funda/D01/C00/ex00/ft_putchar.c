e695a5ffe4541b62cefee0da2d53e9e190071038
Function <42390852+algofunction@users.noreply.github.com>
2024-04-28 21:58:16 +0200

    Update ft_putchar.c

--- Funda/D01/C00/ex00/ft_putchar.c
+++ Funda/D01/C00/ex00/ft_putchar.c
@@ -2,11 +2,19 @@
 Ecrire une fonction affichant un caractère passé en paramètre.
 Pour afficher le caractère, utiliser la fonction write.
 */
-
- 
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void ft_putchar(char c)
 {
-	write(1, &c, 1);
+        write(1, &c, 1);
+}
+
+int main()
+{
+        ft_putchar('a');
+        ft_putchar('\n');
+        putchar('a');
+        putchar('\n');
 }
