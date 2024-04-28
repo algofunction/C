de2a8031be6e68dd9a51d6277b6f9dfbd2af0287
Function <42390852+algofunction@users.noreply.github.com>
2024-04-28 23:58:38 +0200

    Update ft_print_alphabet.c

--- Funda/D01/C00/ex01/ft_print_alphabet.c
+++ Funda/D01/C00/ex01/ft_print_alphabet.c
@@ -2,14 +2,21 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c) {
-  write(1, &c, 1);
+void ft_putchar(char c) {
+        write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void) {
-	char i = 'a';
-	while(i <= 'z') {
-		ft_putchar(i);
-		i++;
-	}
+void ft_print_alphabet(void) {
+        char i = 'a';
+        while(i <= 'z') {
+                ft_putchar(i);
+                i++;
+        }
+}
+
+
+int main(void)
+{
+        ft_print_alphabet();
+        return 0;
 }
