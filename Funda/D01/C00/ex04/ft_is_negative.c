76ffd7d00e5c72dab771e6418439593d5b976fa3
Function <42390852+algofunction@users.noreply.github.com>
2024-05-01 23:49:38 +0200

    Update ft_is_negative.c

--- Funda/D01/C00/ex04/ft_is_negative.c
+++ Funda/D01/C00/ex04/ft_is_negative.c
@@ -5,14 +5,29 @@ Si n est positif ou nul alors afficher 'P'.
 */
 
 #include <unistd.h>
-
-void	ft_is_negative(int n) {
-  if (n >= 0)
-  {
-    write(1, "P", 1);
+ 
+int ft_putchar(char letter) 
+{
+    write( 1, &letter, 1 );
+    
+    return 0;
+}
+ 
+void ft_is_negative (int n)
+{
+    if (n>=0)
+    {
+    	ft_putchar('P');
 	}
-  else
-  {
-    write(1, "N", 1);
+	else
+	{
+		ft_putchar('N');
 	}
+}
+ 
+int main() 
+{
+    ft_is_negative(4568);
+    
+    return 0;
 }
