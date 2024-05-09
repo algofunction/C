74c52755bf85ed133f5c54ea7b95e22e15a61060
Function <42390852+algofunction@users.noreply.github.com>
2024-05-09 09:56:09 +0200

    Update ft_div_mod.c

--- Funda/D01/C01/ex03/ft_div_mod.c
+++ Funda/D01/C01/ex03/ft_div_mod.c
@@ -7,10 +7,26 @@ Elle stocke le reste de la division de a et b dans l’int pointé par mod.
 */
 
 // Solution
-#include <unistd.h>
+#include <stdio.h>
 
-void	ft_div_mod(int a, int b, int *div, int *mod)
+void    ft_div_mod(int a, int b, int *div, int *mod)
 {
-	*div = a / b;
-	*mod = a % b;
+        *div = a / b;
+        *mod = a % b;
+}
+
+int             main(void)
+{
+        int a;
+        int b;
+        int div;
+        int mod;
+
+        a = 10;
+        b = 5;
+        div = 0;
+        mod = 0;
+        ft_div_mod(a, b, &div, &mod);
+        printf("%d\n%d\n", div, mod);
+        return (0);
 }
