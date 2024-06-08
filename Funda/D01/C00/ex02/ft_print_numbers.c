9afd6dc1260171d0f643e58fc52314b6b722ae7f
Function <42390852+algofunction@users.noreply.github.com>
2024-06-08 22:49:07 +0200

    Update ft_print_numbers.c

--- Funda/D01/C00/ex02/ft_print_numbers.c
+++ Funda/D01/C00/ex02/ft_print_numbers.c
@@ -1,29 +1,16 @@
 /* Ecrire une fonction affichant tous les chiffres sur une seule ligne, dans l'ordre croissant.*/
-
 #include <unistd.h>
 
-void	ft_print_numbers(void)
+void    ft_print_numbers(void)
 {
-	short	n;
-
-	n = 47;
-	while (++n < 58)
-		write(1, &n, 1);
+        char    n;
+        n = '/';
+        while (++n < ':')
+                write(1, &n, 1);
 }
 
-void	ft_print_numbers2(void)
+int     main(void)
 {
-	char	n;
-
-	n = '/';
-	while (++n < ':')
-		write(1, &n, 1);
-}
-
-int	main(void)
-{
-	ft_print_numbers();
-	write(1, "\n", 1);
-	ft_print_numbers2();
-	write(1, "\n", 1);
+        ft_print_numbers();
+        write(1, "\n", 1);
 }
