e3c8df8ead5fb3fcf9d1584813a9f7a35dcf4cda
Function <42390852+algofunction@users.noreply.github.com>
2024-07-16 23:59:10 +0200

    Create ft_print_alphabet.c

---
+++ d01/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+void ft_putchar(char i) {
+        write(1, &i, 1);
+}
+
+void ft_print_alphabet(void) {
+        char i = 'a';
+        while(i <= 'z'){
+                ft_putchar(i);
+                i++;
+                '\n';
+        }
+}
+
+char main(void)
+{
+        ft_print_alphabet();
+        return 0;
+}
+~      
