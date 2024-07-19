3fe0e7986e1730d11906fe6e08beece671915361
Function <42390852+algofunction@users.noreply.github.com>
2024-07-19 23:53:56 +0200

    Create ft_print_reverse_alphabet.c

---
+++ d01/ft_print_reverse_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_putchar(char z) {
+        write(1, &z, 1);
+}
+
+void ft_print_reverse_alphabet(void) {
+        char i = 'z';
+        while(i >= 'a') {
+                ft_putchar(i);
+                i--;
+        }
+}
+
+
+int main(void)
+{
+        ft_print_reverse_alphabet();
+        return 0;
+}
