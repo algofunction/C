944e106be47677df3dfc524c70ea02dfaa1dfc4c
Function <42390852+algofunction@users.noreply.github.com>
2024-07-22 23:58:24 +0200

    Create ft_is_negative.c

---
+++ d01/ft_is_negative.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+int ft_putchar(int n)
+{
+    write( 1, &n, 1 );
+    return 0;
+}
+
+void ft_is_negative (int n)
+{
+    if (n>=0)
+    {
+        ft_putchar('P');
+        }
+        else
+        {
+                ft_putchar('N');
+        }
+}
+
+int main()
+{
+        ft_is_negative(4);
+        return 0;
+}
