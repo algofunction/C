a1715d22babd5fbdd0e50da82c775a90c94ad540
Function <42390852+algofunction@users.noreply.github.com>
2024-12-03 00:06:48 +0100

    Update while_1.c

--- d01/while_1.c
+++ d01/while_1.c
@@ -4,10 +4,12 @@ int main()
 {
 int a;
 printf("entrer une valeur entre 50 et 100\n");
-
-b = 2;
-
-while (){
-  a > b;
-  printf(a)
+scanf_s("%d", &a);
+while( a < 50 || a > 100 ){
+  printf("le nombre doit être entre 50 et 100\n");
+  scanf_s("%d", &a);
+  rewind(stdin);
+}
+printf("valeur entree : %d\n", a);
+return 0;
 }
