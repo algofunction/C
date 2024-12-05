cbc225d990c51a329fd2ff6c830d07a5423aa296
Function <42390852+algofunction@users.noreply.github.com>
2024-12-05 19:12:26 +0100

    Update ft_putchar.c

--- d01/ft_putchar.c
+++ d01/ft_putchar.c
@@ -9,3 +9,19 @@ char main()
 {
   ft_putchar('a')
 }
+
+
+#include<unistd.h>
+void ft_putchar(c){
+   write(1, &c, 1)
+}
+
+main(char c)
+{
+ft_putchar()
+}
+
+langage : typé
+avec un void
+string guilemets
+
